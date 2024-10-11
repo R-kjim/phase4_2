5fa017641b9c95488d0dd6ae30ad48cde9ad4ee5
R-kjim <rkayjim@gmail.com>
2024-10-11 20:46:30 +0300

    implements flask api routes

--- server/seed.py
+++ server/seed.py
@@ -8,16 +8,19 @@ with app.app_context():
     # This will delete any existing rows
     # so you can run the seed file multiple times without having duplicate entries in your database
     print("Deleting data...")
-    Pizza.query.delete()
-    Restaurant.query.delete()
-    RestaurantPizza.query.delete()
+    db.drop_all()
+    db.create_all()
+    # Pizza.query.delete()
+    # Restaurant.query.delete()
+    # RestaurantPizza.query.delete()
 
     print("Creating restaurants...")
     shack = Restaurant(name="Karen's Pizza Shack", address='address1')
     bistro = Restaurant(name="Sanjay's Pizza", address='address2')
     palace = Restaurant(name="Kiki's Pizza", address='address3')
     restaurants = [shack, bistro, palace]
-
+    db.session.add_all(restaurants)
+    db.session.commit()
     print("Creating pizzas...")
 
     cheese = Pizza(name="Emma", ingredients="Dough, Tomato Sauce, Cheese")
@@ -26,15 +29,14 @@ with app.app_context():
     california = Pizza(
         name="Melanie", ingredients="Dough, Sauce, Ricotta, Red peppers, Mustard")
     pizzas = [cheese, pepperoni, california]
-
+    db.session.add_all(pizzas)
+    db.session.commit()
     print("Creating RestaurantPizza...")
 
-    pr1 = RestaurantPizza(restaurant=shack, pizza=cheese, price=1)
-    pr2 = RestaurantPizza(restaurant=bistro, pizza=pepperoni, price=4)
-    pr3 = RestaurantPizza(restaurant=palace, pizza=california, price=5)
+    pr1 = RestaurantPizza(restaurant_id=shack.id, pizza_id=cheese.id, price=1)
+    pr2 = RestaurantPizza(restaurant_id=bistro.id, pizza_id=pepperoni.id, price=4)
+    pr3 = RestaurantPizza(restaurant_id=palace.id, pizza_id=california.id, price=5)
     restaurantPizzas = [pr1, pr2, pr3]
-    db.session.add_all(restaurants)
-    db.session.add_all(pizzas)
     db.session.add_all(restaurantPizzas)
     db.session.commit()
 
